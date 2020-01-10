d0636fcced939e797969862a33965627bd22c8fd
Robby Kukurs <robbykukurs@gmail.com>
2020-01-10 20:12:56 +0000

    creating a function to establish aws user session

--- awscontrol.py
+++ awscontrol.py
@@ -2,6 +2,9 @@
 import boto3
 #below is module for establishing low-level aws sessions etc
 import botocore
+#we also have to import an exception from botocore that we're going to catch
+#turns out if an exception is raised within an imported model, python won't just detect it
+from botocore.exceptions import ProfileNotFound
 #below is module for creating CLI commands
 import click
 #below is module for dealing with directories and paths in Python
@@ -27,41 +30,59 @@ def setup_aws_access():
     #close the file when done
     aws_config_file.close()
 
-#USER CONFIRMING THE AWS PROFILE
+#FUNCTION FOR USER CONFIRMING THE AWS PROFILE
 #we're going to ask user which aws profile name they want to use in order to establish session to aws
 #I want to offer the user all available profile names which I think would be cool
+def aws_profile_prompt():
+    #showing Python the correct path to the user's home direcotory
+    path = expanduser("~")
+    #now let us read the aws credentials file and save it to a variabe
+    aws_credentials_file = open(path + "/.aws/credentials", "r")
+    #now let us read the individual lines from the credentials file
+    contents = aws_credentials_file.readlines()
+    #profile name is listed in [] so let's filter those entries and add to a list
+    profile_list = []
+    for i in contents:
+        if "[" and "]" in i:
+            profile_list.append(i)
+    #removing new line characters from end of each list item
+    final_profile_list = []
+    for i in profile_list:
+        final_profile_list.append(i.strip())
+    #now we want to convert the profile name list entries to a nice string so we can
+    #append it to a prompt string later on
+    s = " "
+    s = s.join(final_profile_list)
+    prompt_line = str("You can choose from the following profiles to establish a session to AWS: " + s)
+    print(prompt_line)
 
-#showing Python the correct path to the user's home direcotory
-path = expanduser("~")
-#now let us read the aws credentials file and save it to a variabe
-aws_credentials_file = open(path + "/.aws/credentials", "r")
-#now let us read the individual lines from the credentials file
-contents = aws_credentials_file.readlines()
-#profile name is listed in [] so let's filter those entries and add to a list
-profile_list = []
-for i in contents:
-    if "[" and "]" in i:
-        profile_list.append(i)
-#removing new line characters from end of each list item
-final_profile_list = []
-for i in profile_list:
-    final_profile_list.append(i.strip())
-#now we want to convert the profile name list entries to a nice string so we can
-#append it to a prompt string later on
-s = " "
-s = s.join(final_profile_list)
-prompt_line = str("You can choose from the following profiles: " + s)
-print(prompt_line)
+#FUNCTION FOR ESTABLISHING USER SESSION TO aws - it will only stop when a valid aws profile is selected and session is established
+# check if the aws credentials file exists, if not -  run setup_aws_access first
+# then run while loop for aws credentials file and establish session based on input
+# if incorrect profile name provided - keep within the loop till valid name provided
+def establish_session_to_aws():
+    #let's point towards the user's home direcotory
+    path = expanduser("~")
+    #let's open the aws credentials file, but if it doesn't exist - we need to catch the exception so we have to do a try section
+    try:
+        aws_credentials_file = open(path + "/.aws/credentials", "r")
+    #if aws credentials file doesn't exist; let's catch the exception and setup aws access
+    except FileNotFoundError:
+        setup_aws_access()
 
-#available_aws_profile_names =
-#session_profile_name = input("Please provide your AWS profile name for establishing user session here: ")
-
-#aws session is going to be established by using environmental variables
-#session = boto3.Session(
-#    aws_access_key_id=ACCESS_KEY,
-#    aws_secret_access_key=SECRET_KEY,
-#    aws_session_token=SESSION_TOKEN,
-#)
+    while aws_credentials_file:
+        try:
+            aws_profile_prompt()
+            aws_profile_name = input("Please enter the profile name to use: ")
+            #establishing aws user session using the profile
+            session = boto3.Session(profile_name=str(aws_profile_name))
+        #if user enters non-existent profile name, it will throw an exception; let's catch that and return the loop back to top
+        except ProfileNotFound:
+            print("Sorry, but such an AWS profile can't be found")
+            #if exception caught - jump back to try
+            continue
+        #if session established successfully, exit the while loop
+        break
 
 
 #def cli():
