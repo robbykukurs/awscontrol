77dfb7d8a28ea0b375b7ed5dd04d741752f93a38
Robby Kukurs <robbykukurs@gmail.com>
2020-02-04 06:31:58 +0000

    modified:   awscontrol.py

--- awscontrol.py
+++ awscontrol.py
@@ -2,9 +2,14 @@
 import boto3
 #below is module for establishing low-level aws sessions etc
 import botocore
-#we also have to import an exception from botocore that we're going to catch
+#below 2 modules get imported purely because they will throw exceptions and I will have to catch them
+import urllib3
+import socket
+#we also have to import an exception from any modules that we're going to catch
 #turns out if an exception is raised within an imported model, python won't just detect it
 from botocore.exceptions import ProfileNotFound
+from botocore.exceptions import EndpointConnectionError
+from urllib3.exceptions import NewConnectionError
 #below is module for creating CLI commands
 import click
 #below is module for dealing with directories and paths in Python
@@ -56,50 +61,101 @@ def aws_profile_prompt():
     prompt_line = str("You can choose from the following profiles to establish a session to AWS: " + s)
     print(prompt_line)
 
+#List of all AWS regions to be used when establishing a session
+aws_regions = {
+"US East (Ohio)" : "us-east-2",
+"US East (N. Virginia)" : "us-east-1",
+"US West (N. California)" : "us-west-1",
+"US West (Oregon)" : "us-west-2",
+"Asia Pacific (Hong Kong)" : "ap-east-1",
+"Asia Pacific (Mumbai)" : "ap-south-1",
+"Asia Pacific (Osaka-Local)" : "ap-northeast-3",
+"Asia Pacific (Seoul)" : "ap-northeast-2",
+"Asia Pacific (Singapore)" : "ap-southeast-1",
+"Asia Pacific (Sydney)" : "ap-southeast-2",
+"Asia Pacific (Tokyo)" : "ap-northeast-1",
+"Canada (Central)" : "ca-central-1",
+"Europe (Frankfurt)" : "eu-central-1",
+"Europe (Ireland)" : "eu-west-1",
+"Europe (London)" : "eu-west-2",
+"Europe (Paris)" : "eu-west-3",
+"Europe (Stockholm)" : "eu-north-1",
+"Middle East (Bahrain)" : "me-south-1",
+"South America (São Paulo)" : "sa-east-1",
+}
+
+#asking user to input the number that corresponds with the AWS region they want to connect to
+def aws_region_prompt():
+    #giving user options for AWS regions
+    print("These are all available AWS regions")
+    for i in aws_regions:
+        print("For " + str(i) + " enter " + str(aws_regions[i]))
+
 ####### THIS IS WHERE THE SCRIPT ACTUALLY STARTS RUNNING #######
+##### SESSION IS GETTING ESTABLISHED EVERY TIME SCRIPT RUNS SO THAT WE KNOW WHICH PROFILE TO USE #####
+#let's point towards the user's home direcotory
+
+path = expanduser("~")
+#let's open the aws credentials file, but if it doesn't exist - we need to catch the exception so we have to do a try section
+try:
+    aws_credentials_file = open(path + "/.aws/credentials", "r")
+#if aws credentials file doesn't exist; let's catch the exception and setup aws access
+except FileNotFoundError:
+    setup_aws_access()
+
+#function for establishing a user session to AWS and getting all available resources
+def connect_to_aws():
+    while aws_credentials_file:
+        try:
+            aws_profile_prompt()
+            aws_profile_name = input("Please enter the profile name to use: ")
+            aws_region_prompt()
+            aws_region_name = input("Please enter the AWS region of your choice: ")
+            #as per https://boto3.amazonaws.com/v1/documentation/api/latest/guide/session.html
+            session = boto3.session.Session(profile_name=str(aws_profile_name), region_name=str(aws_region_name))
+            #pulling all availableresources from the established session https://boto3.amazonaws.com/v1/documentation/api/latest/_modules/boto3/session.html#Session.get_available_services
+            resource_list = session.get_available_resources()
+            service_list = session.get_available_services()
+            s3 = session.resource('s3')
+            ec2 = session.resource('ec2')
+            #if user enters non-existent profile name, it will throw an exception; let's catch that and return the loop back to top
+        except ProfileNotFound:
+            print("Sorry, but such an AWS profile can't be found")
+            #if exception caught - jump back to try
+            continue
+            #if user enters non-existent aws region name, it will throw 3 exceptions in a row, let's catch that and return the loop back to top
+        except (socket.gaierror, NewConnectionError, EndpointConnectionError):
+            print("Sorry, but such an AWS region doesn't exist")
+            #if exception caught - jump back to try
+            continue
+            #if session established successfully, exit the while loop
+        break
 
 #here we're naming the main @click group - click is a module helping us
 #with creation of CLI options and commands - it's imported on very top
 @click.group()
 def aws_control():
-    """Robby Kukurs' Python CLI tool to manage AWS resources. Please use "user connect" command to establish session first"""
+    """Robby Kukurs' Python CLI tool to manage AWS resources. Please use one of the below options to manage a specific AWS resource"""
 
-#here we're naming the "user" command
-@aws_control.group('user')
-def user():
-    """Commands for connecting to AWS and establishing a user session. Use the "connect" option to start session"""
+@aws_control.group('s3')
+def s3():
+    """Commands for managing S3 in your AWS accont"""
 
-@user.command('connect')
-#FUNCTION FOR ESTABLISHING USER SESSION TO aws - it will only stop when a valid aws profile is selected and session is established
-# check if the aws credentials file exists, if not -  run setup_aws_access first
-# then run while loop for aws credentials file and establish session based on input
-# if incorrect profile name provided - keep within the loop till valid name provided
+@s3.command('list')
+def list_s3_bucket():
+    connect_to_aws()
+    for bucket in s3.buckets.all():
+        print(bucket.name)
 
-#below function can be called whatever you want, it will automatically go with
-#above @click user.command - so sequence is very important here
-def establish_session_to_aws():
-    #let's point towards the user's home direcotory
-    path = expanduser("~")
-    #let's open the aws credentials file, but if it doesn't exist - we need to catch the exception so we have to do a try section
-    try:
-        aws_credentials_file = open(path + "/.aws/credentials", "r")
-    #if aws credentials file doesn't exist; let's catch the exception and setup aws access
-    except FileNotFoundError:
-        setup_aws_access()
+@aws_control.group('ec2')
+def ec2():
+    """Commands for managing VPC in your AWS accont"""
 
-    while aws_credentials_file:
-        try:
-            aws_profile_prompt()
-            aws_profile_name = input("Please enter the profile name to use: ")
-            #establishing aws user session using the profile
-            session = boto3.Session(profile_name=str(aws_profile_name))
-        #if user enters non-existent profile name, it will throw an exception; let's catch that and return the loop back to top
-        except ProfileNotFound:
-            print("Sorry, but such an AWS profile can't be found")
-            #if exception caught - jump back to try
-            continue
-        #if session established successfully, exit the while loop
-        break
+@ec2.command('list')
+def list_ec2_instances():
+    connect_to_aws()
+    for instance in ec2.instances.all():
+        print(instance.name)
 
 if __name__ == '__main__':
     aws_control()
